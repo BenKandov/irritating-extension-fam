f67c6e221b82d298c01ce8b08ec1335b13b900e9
BenKandov <benjamin.kandov@stonybrook.edu>
2015-09-05 17:36:23 -0400

    initial commit
    irritating chrome extension

---
+++ src/inject/inject.js
@@ -0,0 +1,51 @@
+
+
+
+
+chrome.extension.sendMessage({}, function(response) {
+    var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+        var song = new Audio('src/deathgrips-esque.mp3');
+        song.play();
+        $('*').css('background-color','cyan');
+		$('*').css('fontSize','large');
+		$('*').css('fontFamily','Comic sans');
+		$('*').css('font-color','red');
+
+        // Walk through all the nodes to get all text nodes.
+        // Thanks to: http://stackoverflow.com/a/5904945
+        // and also: http://stackoverflow.com/a/9452386
+        var textNodes = [];
+        (function walkNodeForText(node) {
+            if (node) {
+                node = node.firstChild;
+                while (node != null) {
+                    switch (node.nodeType) {
+                        // Recurse into elements, documents, and document fragments
+                        case 1:
+                        case 9:
+                        case 11:
+                            walkNodeForText(node);
+                            break;
+                        // Add text nodes to the list of elements we want to modify
+                        case 3:
+                            textNodes.push(node);
+                            break;
+                    }
+
+                    node = node.nextSibling;
+                }
+            }
+        })(document.body);
+
+        textNodes.forEach(function(currentVal, index, array) {
+            // replace all case-insensitive occurences of 'cloud' with 'butt'.
+            currentVal.nodeValue = currentVal.nodeValue.replace(/images/gi, "CHECK OUT MAH MIXTAPE FAM ");
+            currentVal.nodeValue = currentVal.nodeValue.replace(/search/gi, "CHECK OUT MAH MIXTAPE FAM ");
+            currentVal.nodeValue = currentVal.nodeValue.replace(/ /gi, "CHECK OUT MAH MIXTAPE FAM ");
+        });
+
+    }
+    }, 10);
+});
